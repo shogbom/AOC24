30099c7be239d87e7dc278c42566531292c9c9cb
shogbom <samuel.aronson.hogbom@gmail.com>
2024-12-01 21:21:36 +0100

    second task

---
+++ d1/second.cc
@@ -0,0 +1,42 @@
+#include <fstream>
+#include <vector>
+#include <iostream>
+#include <string>
+#include <climits>
+
+using namespace std;
+
+int main()
+{
+    int in;
+    ifstream file("list");
+    vector<int> left;
+    vector<int> right;
+    bool leftcheck = true;
+
+    while (file >> in)
+    {
+        if (leftcheck)
+        {
+            left.push_back(in);
+            leftcheck = !leftcheck;
+        }
+        else
+        {
+            right.push_back(in);
+            leftcheck = !leftcheck;
+        }
+    }
+    unsigned int sim{0};
+    unsigned int times{0};
+    for(unsigned int i{0}; i < left.size(); i++){
+        times = 0;
+        for(unsigned int k{0}; k < right.size(); k++){
+            if(left[i] == right[k]){
+                times++;
+            }
+        }
+        sim += left[i] * times;
+    }
+    cout << sim << endl;
+}
