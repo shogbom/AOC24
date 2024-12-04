e5a767c35608b7bb4794335eb48852ae7fbeff47
shogbom <samuel.aronson.hogbom@gmail.com>
2024-12-04 16:16:14 +0100

    day 4 task 2

--- d4/second.cc
+++ d4/second.cc
@@ -19,6 +19,7 @@ int main()
     ifstream file("list");
     string in;
     vector<string> data;
+    int counta{0};
 
     while(getline(file, in)){
         data.push_back(in);
@@ -27,15 +28,42 @@ int main()
     int count{0};
     for(unsigned int i{0}; i < data.size(); ++i){
         for(unsigned int ii{0}; ii < data[i].length(); ii++){
-            //if((data[i][ii] == 'X') || (data[i][ii] == 'S')){
+            if((data[i][ii] == 'A')){
+                counta++;
                 count += x(data, i, ii);
-            //}
+            }
         }
     }
+    cout << counta << endl;
     cout << count << endl;
+    file.close();
 }
 
-int x(vector<string>, unsigned int x, unsigned int y)
+int x(vector<string> data, unsigned int x, unsigned int y)
 {
-    return 0;
+    string temp1{""}, temp2{""};
+
+    if(((x-1) < data.size()) && ((y-1) < data[x-1].length()) && ((x+1) < data.size()) && ((y+1) < data[x+1].length()) && ((y-1) < data[x+1].length()) && ((y+1) < data[x+1].length())){ // readable perfection.
+        temp1 += data[x-1][y-1];
+        temp1 += data[x][y];
+        temp1 += data[x+1][y+1];
+
+        temp2 += data[x - 1][y + 1];
+        temp2 += data[x][y];
+        temp2 += data[x+1][y-1];
+        
+    }
+
+    if( ((temp1 == "MAS") || (temp1 == "SAM")) && ((temp2 == "MAS")|| (temp2 == "SAM"))){ // dubble if for ease of debug.
+
+        return 1;
+    } else {
+        cout << temp1 << endl;
+        cout << temp2 << endl;
+        cout << x << "  " << y << endl;
+        return 0;
+    }
 }
+
+
+//1796 e too low. tydligen inte ****.
